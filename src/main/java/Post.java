bbf4b304d5c330c1b8d4239eeec48e95a98eff98
Olga Zotova <olgazotova1976@yandex.ru>
2025-09-05 09:17:16 +0300

    Modified class Post

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -4,9 +4,6 @@ public class Post {
     public String patronymic; // поле для отчества
     public String passport; // поле для номера паспорта
     public String phone; // поле для номера телефона
-    public int day;   // день рождения
-    public int month; // месяц рождения
-    public int year;  // год рождения
     public FormDate birthday;
     public boolean subscription; // поле для признака подписки
 }
